39db072c84e31fe846037dbdd86fe1e870ccba3a
Dmitry <durexrulez@gmail.com>
2016-03-11 21:25:51 +0200

    jpg png

---
+++ src/js/map.js
@@ -0,0 +1,14 @@
+function initialize() {
+	 var latlng = new google.maps.LatLng(59.936129, 30.320955);
+	 var settings = {
+	 zoom: 15,
+	 center: latlng,
+	 mapTypeControl: true,
+	 mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+	 navigationControl: true,
+	 navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+	 mapTypeId: google.maps.MapTypeId.ROADMAP
+	 };
+	var map = new google.maps.Map(document.querySelector(".map"), settings);
+};
+initialize();

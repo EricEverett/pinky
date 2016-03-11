65c5ab8fc45c54d7e2c90ec97ed05a260e8e7f7b
Dmitry <durexrulez@gmail.com>
2016-03-11 21:23:13 +0200

    bootstrap mixins and grid less

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,33 @@ module.exports = function(grunt) {
 	require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
+
+  	// Files 
+  	copy: {
+  		build:{
+  			files:[{
+  				expand:true,
+  				cwd: "src",
+  				src: [
+  					"img/**",
+  					"css/**",
+  					"js/**",
+  					"index.html"
+  				],
+  				dest: "build"
+  			}]
+  		}
+  	},
+
+  	clean:{
+  		build:["build"]
+  	},
+
+  	//CSS
   	csscomb:{
   		style:{
   			expand:true,
-  			src:["src/less/**/*.less"]
+  			src:["src/less/components/*.less"]
   		}
   	},
 
@@ -56,12 +79,13 @@ module.exports = function(grunt) {
   	uglify:{
   		js:{
   			files:{
-  				"build/js/script.min.js":["src/js/file1.js", "src/js/file2.js"]
+  				"build/js/script.min.js":["build/js/grunticon.loader.js","build/js/map.js"]
   			}
   		}
   	},
-  	
+
   	// IMAGE
+
   	imagemin:{
   		images:{
   			options: {
@@ -69,21 +93,86 @@ module.exports = function(grunt) {
   			},
   			files:[{
   				expand:true,
-  				src: ["src/img/**/*.{png,jpg,gif,svg}"]
+  				src: ["build/img/**/*.{png,jpg,gif,svg}"]
+  			}]
+  		}
+  	},
+  	grunticon: {
+	    myIcons: {
+	        files: [{
+	            expand: true,
+	            cwd: 'src/_svg',
+	            src: ['**/*.svg', '**/*.png'],
+	            dest: "src/img"
+	        }],
+	        options: {
+	          enhanceSVG: true, // style and animate with CSS or add interactivity with JS
+	          datasvgcss : '../css/grunticon-icons.data.svg.css',
+	          datapngcss : '../css/grunticon-icons.data.png.css',
+	          urlpngcss : '../css/grunticon-icons.fallback.css',
+	          previewhtml : '../_svg/_preview.html',
+	         // loadersnippet: 'grunticon.loader.js',
+	        // имя папки, в которую будут записаны PNG
+	          pngfolder : 'png',
+	            pngpath : 'png',
+	          cssprefix: ".icon-",
+
+	          template : 'src/_svg/custom-css.hbs',
+	          defaultWidth : '20px',
+	          defaultHeight: '20px'
+        }
+	    }
+		},
+
+  	//HTML 
+  	includereplace:{
+  		include:{
+  			options:{
+
+  			},
+  			src: "src/*.html",
+  			dest: "build/" //src copy to build !!!!
+  		}
+  	},
+
+  	// for src slashes
+  	replace:{
+  		build:{
+  			options:{
+  				patterns:{
+  					match: /[\"\']img\//g,
+  					replacement: '"/img'
+  				}
+  			},
+  			files:[{
+  				expand:true,
+  				src:[
+  					"build/css/style*.css",
+  					"build/index*.html"
+  					]
   			}]
   		}
   	}
-
     // pkg: grunt.file.readJSON('package.json')
 
   });
 
   grunt.registerTask("build", [
+  	"clean",
+  	"copy",
   	"csscomb",
   	"less",
   	"autoprefixer",
   	"cmq",
-  	"cssmin"
+  	"cssmin",
+  	"uglify",
+  	"includereplace"
+  ]);
+	
+	// for image optimization  
+  grunt.registerTask("image", [
+  	"grunticon",
+  	"imagemin"
   ]);
 
 

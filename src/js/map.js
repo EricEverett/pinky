4cd2bf72a9cc9fdc77710eb874b242e4ed848449
Dmitry <durexrulez@gmail.com>
2016-03-12 15:48:47 +0200

    base typography/ component pre table-price

--- src/js/map.js
+++ src/js/map.js
@@ -1,7 +1,7 @@
 function initialize() {
 	 var latlng = new google.maps.LatLng(59.936129, 30.320955);
 	 var settings = {
-	 zoom: 15,
+	 zoom: 17,
 	 center: latlng,
 	 mapTypeControl: true,
 	 mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
@@ -10,5 +10,17 @@ function initialize() {
 	 mapTypeId: google.maps.MapTypeId.ROADMAP
 	 };
 	var map = new google.maps.Map(document.querySelector(".map"), settings);
+
+	// Marker
+	var markerCircle = new google.maps.Circle({
+      strokeColor: '#ffffff',
+      strokeOpacity: 1,
+      strokeWeight: 12,
+      fillColor: '#d22856',
+      fillOpacity: 1,
+      map: map,
+      center: {lat: 59.936129, lng: 30.320955},
+      radius: 15
+    });
 };
 initialize();
